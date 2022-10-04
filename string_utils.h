b570e226d845ef5b89a0ca3767f4afd49b7b6062
severin-denisenko <devchonkavkorobchonke@gmail.com>
2022-10-04 18:56:14 +0300

    Type mismatch.

--- string_utils.h
+++ string_utils.h
@@ -11,11 +11,11 @@
 #define MAX_BUFFER_LENGTH 256
 #define MAX_PARAMETERS 256
 
-void remove_spaces(char *s);
+void remove_spaces(char s[MAX_BUFFER_LENGTH]);
 
-void remove_comments(char *s);
+void remove_comments(char s[MAX_BUFFER_LENGTH]);
 
-void split_parameters(const char *s, char *p, char *v);
+void split_parameters(const char s[MAX_BUFFER_LENGTH], char p[MAX_BUFFER_LENGTH], char v[MAX_BUFFER_LENGTH]);
 
 int is_in_parameters(char arr[MAX_PARAMETERS][MAX_PARAMETERS], const char *s);
 
